98f4661256aa4a8746a35860f39756ec6d4657fe
Kevin Smyth <kevin.m.smyth@gmail.com>
2017-04-28 08:46:27 -0500

    Fix typo

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ config.webhooks.enable = true
 // webgme-rdf configuration:
 config.webgme_rdf = {
     // Fuseki URL
-    RdfServerUrl: 'http://localhost::8890',
+    RdfServerUrl: 'http://localhost:8890',
     ListenPort: 8080,
     // Named graphs start with this:
     WebGmeServerUrl: 'http://localhost:8888'
